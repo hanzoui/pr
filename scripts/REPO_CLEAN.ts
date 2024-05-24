71636d8171a3f693bc701ff3e3d489e6676d455a
snomiao <snomiao@gmail.com>
2024-05-24 07:12:53 +0000

    fix(main): demo clean repo
    demo clean repo

---
+++ scripts/REPO_CLEAN.ts
@@ -0,0 +1,32 @@
+import DIE from "@snomiao/die";
+import { Octokit } from "octokit";
+import { question } from "zx";
+const GH_TOKEN_FOR_DELETE_REPO =
+  process.env.GH_TOKEN_FOR_DELETE_REPO ||
+  DIE(
+    "Missing env.GH_TOKEN_FOR_DELETE_REPO" +
+      "get one from https://github.com/settings/tokens?type=beta"
+  );
+const octokit = new Octokit({ auth: GH_TOKEN_FOR_DELETE_REPO });
+export const gh = octokit.rest;
+
+const deleteList = [
+  "drip-art/PR-ComfyNode-Registry-test-9d050f95",
+  "drip-art/PR-ComfyNode-Registry-test-c1020b39",
+  "drip-art/PR-ComfyNode-Registry-test-720a7a47",
+  "drip-art/PR-ComfyNode-Registry-test-fe0677d1",
+  "drip-art/PR-ComfyNode-Registry-test-a996033a",
+  "drip-art/PR-ComfyNode-Registry-test-f84e1a5d",
+  "drip-art/PR-ComfyNode-Registry-test-a7ab0504",
+  "drip-art/PR-ComfyNode-Registry-test-c7ecc72a",
+  "drip-art/PR-ComfyNode-Registry-test-c099f89f",
+];
+
+for await (const path of deleteList) {
+  const [owner, repo] = path.split("/");
+  await gh.repos.delete({
+    owner,
+    repo,
+  });
+  console.log(`${path} DELETE OK`);
+}
--- scripts/gh.ts
+++ scripts/gh.ts
@@ -5,5 +5,5 @@ const GH_TOKEN =
   process.env.GH_TOKEN ||
   (await question("Input GH_TOKEN from https://github.com/settings/tokens?type=beta: ")) ||
   DIE("Missing env.GH_TOKEN from https://github.com/settings/tokens?type=beta");
-const octokit = new Octokit({ auth: GH_TOKEN });
+const octokit = new Octokit({ auth: GH_TOKEN ,});
 export const gh = octokit.rest;
