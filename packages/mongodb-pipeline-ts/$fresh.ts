c76752ee02ee0904118eb88016c2d0f4b1f98810
snomiao <snomiao@gmail.com>
2024-06-18 02:06:15 +0000

    fix(pipeline): types use array-path
    types use array-path

--- packages/mongodb-pipeline-ts/$fresh.ts
+++ packages/mongodb-pipeline-ts/$fresh.ts
@@ -7,6 +7,7 @@ type $fresh = { $gte: Date };
 export function $stale(at: Date): $stale;
 export function $stale(interval: number): $stale;
 export function $stale(duration: string): $stale;
+export function $stale(x: Date | number | string): $stale;
 export function $stale(x: Date | number | string) {
   if (typeof x === "string") return $stale(enhancedMs(x));
   if (typeof x === "number") return $stale(new Date(+new Date() - x));
@@ -15,6 +16,7 @@ export function $stale(x: Date | number | string) {
 export function $fresh(at: Date): $fresh;
 export function $fresh(interval: number): $fresh;
 export function $fresh(duration: string): $fresh;
+export function $fresh(x: Date | number | string): $fresh;
 export function $fresh(x: Date | number | string) {
   if (typeof x === "string") return $fresh(enhancedMs(x));
   if (typeof x === "number") return $fresh(new Date(+new Date() - x));
@@ -24,17 +26,19 @@ export function $fresh(x: Date | number | string) {
 export function $before(at: Date): $before;
 export function $before(interval: number): $before;
 export function $before(duration: string): $before;
+export function $before(x: Date | string | number): $before;
 export function $before(x: Date | string | number) {
   if (typeof x === "string") return $before(enhancedMs(x));
-  if (typeof x === "number") return $before(new Date(x));
+  if (typeof x === "number") return $before(new Date(+new Date() - x));
   return { $lt: x };
 }
 
 export function $after(at: Date): $after;
 export function $after(interval: number): $after;
 export function $after(duration: string): $after;
+export function $after(x: Date | string | number): $after;
 export function $after(x: Date | string | number) {
   if (typeof x === "string") return $after(enhancedMs(x));
-  if (typeof x === "number") return $after(new Date(x));
+  if (typeof x === "number") return $after(new Date(+new Date() - x));
   return { $gt: x };
 }
