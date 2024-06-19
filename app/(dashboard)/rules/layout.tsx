0fb7aa3e1abde412a80b4f7bd59a3b85ed847e95
snomiao <snomiao@gmail.com>
2024-06-19 08:08:23 +0000

    feat(follow-up-rules): editor
    editor

--- app/(dashboard)/rules/layout.tsx
+++ app/(dashboard)/rules/layout.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 export default async function RulesLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-wrap">
-
       <div className="grow">{children}</div>
     </div>
   );
