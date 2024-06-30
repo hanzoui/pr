7f4cd3ec49336d5ba59519ac90e369b7fe458741
haohaocreates <yechenghao.du@gmail.com>
2024-06-30 00:47:07 -0400

    query for custom nodes

--- csv-query.js
+++ csv-query.js
@@ -4,27 +4,53 @@ import csv from 'csv-parser';
 const staleLimit = 180;
 
 const processCSV = (filePath) => {
-    const repoValues = [];
+    let total = 0;
+    let staleCount = 0;
+    let onRegistry = 0;
+    let mergedOrClosed = 0;
+    let open = 0;
+    let openZero = 0;
+    let openOne = 0;
+    let openTwo = 0;
+
 
     fs.createReadStream(filePath).pipe(csv()).on('data', (row) => {
-        if(row.repo_updated){
-            repoValues.push(row.repo_updated);
+        if(total === 0)
+            console.log("row:" , row);
+        if(convertToDays(row.repo_updated) >= staleLimit)
+            staleCount++;        
+        if(row.on_registry === 'true')
+            onRegistry++;
+        if((row.state === 'MERGED' || row.state === 'CLOSED') && row.on_registry === 'false')
+            mergedOrClosed++;
+        if(row.state === 'OPEN' && row.on_registry === 'false'){
+            // console.log("row.comments: ", row.comments)
+            open++;
+            if(row.comments == 0){openZero++;
+                // console.log("row: ", row)
+            }
+            if(row.comments == 1)openOne++;
+            if(row.comments >= 2)openTwo++;
         }
         
+            
+            
+        total++;
+        
     }).on(`end`, () => {
-        const activeRepos = repoValues.filter(value => {
-            const days = convertToDays(value);
-            return days < staleLimit
-        })
+        console.log("Total Nodes: ", Math.floor(total/2));
+        console.log("Total Stale Nodes: ", Math.floor(staleCount/2));
+        console.log("Total Active Nodes: ", Math.floor(total/2) - Math.floor(staleCount/2));
+        console.log("-------------------------------------")
+        console.log("On Registry: ", Math.floor(onRegistry/2));
+        console.log("Open PRs: ", Math.floor(open/2))
+        console.log("Merged/Closed: ", Math.floor(mergedOrClosed/2))
+        console.log("-------------------------------------")
+        console.log("No FU Yet (Sending to Rule 1): ", Math.floor(openZero/2))
+        console.log("1 PR Comment on GH (Sending to Rule 2): ", Math.floor(openOne/2))
+        console.log("2+ PR Comments --> Other Social Media (Sending to Rule 3): ", Math.floor(openTwo/2))
 
-        const staleRepos = repoValues.filter(value => {
-            const days = convertToDays(value);
-            return days >= staleLimit
-        })
-
-        console.log("Total Repos: ", repoValues.length/2);
-        console.log("Active Repos: ", activeRepos.length/2);
-        console.log("Stale Repos: ", staleRepos.length/2);
+        // console.log(Math.floor(onRegistry/2) + Math.floor(open/2) + Math.floor(mergedOrClosed/2))
     })
 }
 
