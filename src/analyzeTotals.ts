65f654ed4c4826e69aaabb0d1dc642028e31492a
haohaocreates <yechenghao.du@gmail.com>
2024-07-03 20:58:55 -0400

    divide dashboard page into sections

--- src/analyzeTotals.ts
+++ src/analyzeTotals.ts
@@ -25,52 +25,52 @@ export async function analyzeTotals() {
       "on ComfyUI Manager": CMNodes.estimatedDocumentCount(),
       "on Registry": CRNodes.estimatedDocumentCount(),
     }),
-    "Total Repos": $pipeline(CNRepos)
-      .group({
-        _id: null,
-        "on Comfy Manager List": { $sum: { $cond: [{ $eq: [{ $type: "$cm" }, "missing"] }, 0, 1] } },
-        "on Registry": { $sum: { $cond: [{ $eq: [{ $type: "$cr" }, "missing"] }, 0, 1] } },
-        Archived: { $sum: { $cond: ["$info.data.archived", 1, 0] } },
-        All: { $sum: 1 },
-        // Candidates: { $sum: { $cond: ["$candidate.data", 1, 0] } },
-        "Got ERROR on creating PR": { $sum: { $cond: [{ $eq: ["$createdPulls.state", "error"] }, 1, 0] } },
-      })
-      .project({ _id: 0 })
-      .aggregate()
-      // .map((e: any) => e.pairs)
-      .next(),
-    "Total Authors": $pipeline(CNRepos)
-      .match($filaten({ info: { data: { owner: { login: { $exists: true } } } } }))
-      .group({
-        _id: "$info.data.owner.login",
-        // author: "$info.data.owner.login",
-        cm: { $sum: { $cond: [{ $eq: [{ $type: "$cm" }, "missing"] }, 0, 1] } },
-        cr: { $sum: { $cond: [{ $eq: [{ $type: "$cr" }, "missing"] }, 0, 1] } },
-        All: { $sum: 1 },
-      })
-      .set({ author: "$_id" })
-      .group({
-        _id: null,
-        "on Comfy Manager List": { $sum: { $cond: [{ $eq: ["$cm", 0] }, 0, 1] } },
-        "on Registry": { $sum: { $cond: [{ $eq: ["$cr", 0] }, 0, 1] } },
-        All: { $sum: 1 },
-      })
-      .project({ _id: 0 })
-      .aggregate()
-      // .map((e: any) => e.pairs)
-      .next(),
-    "Total PRs Made": $pipeline(CNRepos)
-      .unwind("$crPulls.data")
-      .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
-      .sort({ _id: 1 })
-      .set({ id_total: [["$_id", "$total"]] })
-      .group({ _id: null, pairs: { $mergeObjects: { $arrayToObject: "$id_total" } } })
-      .aggregate()
-      .map((e: any) => e.pairs)
-      .next(),
+    // "Total Repos": $pipeline(CNRepos)
+    //   .group({
+    //     _id: null,
+    //     "on Comfy Manager List": { $sum: { $cond: [{ $eq: [{ $type: "$cm" }, "missing"] }, 0, 1] } },
+    //     "on Registry": { $sum: { $cond: [{ $eq: [{ $type: "$cr" }, "missing"] }, 0, 1] } },
+    //     Archived: { $sum: { $cond: ["$info.data.archived", 1, 0] } },
+    //     All: { $sum: 1 },
+    //     // Candidates: { $sum: { $cond: ["$candidate.data", 1, 0] } },
+    //     "Got ERROR on creating PR": { $sum: { $cond: [{ $eq: ["$createdPulls.state", "error"] }, 1, 0] } },
+    //   })
+    //   .project({ _id: 0 })
+    //   .aggregate()
+    //   // .map((e: any) => e.pairs)
+    //   .next(),
+    // "Total Authors": $pipeline(CNRepos)
+    //   .match($filaten({ info: { data: { owner: { login: { $exists: true } } } } }))
+    //   .group({
+    //     _id: "$info.data.owner.login",
+    //     // author: "$info.data.owner.login",
+    //     cm: { $sum: { $cond: [{ $eq: [{ $type: "$cm" }, "missing"] }, 0, 1] } },
+    //     cr: { $sum: { $cond: [{ $eq: [{ $type: "$cr" }, "missing"] }, 0, 1] } },
+    //     All: { $sum: 1 },
+    //   })
+    //   .set({ author: "$_id" })
+    //   .group({
+    //     _id: null,
+    //     "on Comfy Manager List": { $sum: { $cond: [{ $eq: ["$cm", 0] }, 0, 1] } },
+    //     "on Registry": { $sum: { $cond: [{ $eq: ["$cr", 0] }, 0, 1] } },
+    //     All: { $sum: 1 },
+    //   })
+    //   .project({ _id: 0 })
+    //   .aggregate()
+    //   // .map((e: any) => e.pairs)
+    //   .next(),
+    // "Total PRs Made": $pipeline(CNRepos)
+    //   .unwind("$crPulls.data")
+    //   .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
+    //   .sort({ _id: 1 })
+    //   .set({ id_total: [["$_id", "$total"]] })
+    //   .group({ _id: null, pairs: { $mergeObjects: { $arrayToObject: "$id_total" } } })
+    //   .aggregate()
+    //   .map((e: any) => e.pairs)
+    //   .next(),
     "Total Open": $pipeline(CNRepos)
       .unwind("$crPulls.data")
-      .match({ "crPulls.data.pull.prState": "open" })
+      .match({ cr: { $exists: true }, "crPulls.data.pull.prState": "open", "crPulls.data.type": "pyproject" })
       .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
       .sort({ _id: 1 })
       .set({ id_total: [["$_id", "$total"]] })
@@ -80,7 +80,7 @@ export async function analyzeTotals() {
       .next(),
     "Total Merged (on Registry)": $pipeline(CNRepos)
       .unwind("$crPulls.data")
-      .match({ "crPulls.data.pull.prState": "merged" })
+      .match({ cr: { $exists: true }, "crPulls.data.pull.prState": "merged", "crPulls.data.type": "pyproject" })
       .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
       .sort({ _id: 1 })
       .set({ id_total: [["$_id", "$total"]] })
@@ -88,16 +88,6 @@ export async function analyzeTotals() {
       .aggregate()
       .map((e: any) => e.pairs)
       .next(),
-    // "Total Merged (on Registry)": $pipeline(CNRepos)
-    //   .unwind("$crPulls.data")
-    //   .match({ cr: { $exists: true }, "crPulls.data.pull.prState": "merged" })
-    //   .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
-    //   .sort({ _id: 1 })
-    //   .set({ id_total: [["$_id", "$total"]] })
-    //   .group({ _id: null, pairs: { $mergeObjects: { $arrayToObject: "$id_total" } } })
-    //   .aggregate()
-    //   .map((e: any) => e.pairs)
-    //   .next(),
     // "Total Merged (not on Registry)": $pipeline(CNRepos)
     //   .unwind("$crPulls.data")
     //   .match({ cr: { $exists: false }, "crPulls.data.pull.prState": "merged" })
@@ -110,7 +100,17 @@ export async function analyzeTotals() {
     //   .next(),
     "Total Closed": $pipeline(CNRepos)
       .unwind("$crPulls.data")
-      .match({ "crPulls.data.pull.prState": "closed" })
+      .match({ cr: { $exists: true }, "crPulls.data.pull.prState": "closed", "crPulls.data.type": "pyproject" })
+      .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
+      .sort({ _id: 1 })
+      .set({ id_total: [["$_id", "$total"]] })
+      .group({ _id: null, pairs: { $mergeObjects: { $arrayToObject: "$id_total" } } })
+      .aggregate()
+      .map((e: any) => e.pairs)
+      .next(),
+      "----------------------\nNo FU Yet (Sending to Rule 1):": $pipeline(CNRepos)
+      .unwind("$crPulls.data")
+      .match({ cr: { $exists: false }, "crPulls.data.pull.prState": "open", "crPulls.data.type": "pyproject", "crPulls.data.pulls" })
       .group({ _id: "$crPulls.data.type", total: { $sum: 1 } })
       .sort({ _id: 1 })
       .set({ id_total: [["$_id", "$total"]] })
