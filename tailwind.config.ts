2e9c135402dae745900ad7a8c62f73f34c11ffd3
snomiao <snomiao@gmail.com>
2024-08-09 00:23:22 +0000

    fix(main): restore daisyui
    restore daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,7 +69,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("daisyui"), require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
