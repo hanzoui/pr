85638c12ff385bd68d4f76ba84d176a1aec21267
snomiao <snomiao@gmail.com>
2024-08-15 06:36:35 +0000

    🐛 fix: Remove unnecessary code related to test API functionality
    - Deleted isTestApiUp.ts file as it is no longer needed for the application.
    - Updated comments in route.test.ts to reflect changes in the test structure.
    - Removed unnecessary code blocks related to test handling to streamline the testing process.

--- app/api/[...openapi]/route.test.ts
+++ app/api/[...openapi]/route.test.ts
@@ -1,12 +1,19 @@
-// bun dev before running this test
+// // bun dev before running this test
 import pkg from "@/package.json";
 import { expect, it } from "bun:test";
 import { isTestApiUp } from "./isTestApiUp";
 import { testApiBase } from "./testApiBase";
 
-it.skipIf(!isTestApiUp);
+// // it.skipIf(!isTestApiUp);
 it.skip("should fetch version", async () => {
   const response = await fetch(testApiBase + "/version");
   expect(response.status).toBe(200);
   expect(await response.json()).toEqual({ version: pkg.version });
 });
+
+// // export const isTestApiUp = await fetch(testApiBase + "/version")
+// //   .then(() => true)
+// //   .catch(() => {
+// //     console.error("Nextjs Server is not running, skip api tests, run `bun dev` to start server for api tests");
+// //     return null;
+// //   });
