5d4e55527f3bcc0d1f88c66a95f46a652ed91876
snomiao <snomiao@gmail.com>
2024-12-31 10:23:20 +0000

    feat: add an organisation or owner check to run the action
    - [Forks problem: add an organisation or owner check to run the action · Issue #2 · Comfy-Org/publish-node-action]( https://github.com/Comfy-Org/publish-node-action/issues/2#issuecomment-2557632871 )

--- src/GIT_USEREMAIL.ts
+++
@@ -1,5 +0,0 @@
-import DIE from "@snomiao/die";
-import { ghUser } from "./ghUser";
-
-export const GIT_USEREMAIL =
-  process.env.GIT_USEREMAIL || (ghUser.email && ghUser.email) || DIE("Missing env.GIT_USEREMAIL");
