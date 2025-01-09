5d68db9214e128cc19784333f1a77583a952be5b
snomiao <snomiao@gmail.com>
2025-01-09 12:52:52 +0000

    fix(details): format
    format

--- app/(dashboard)/details/page.tsx
+++ app/(dashboard)/details/page.tsx
@@ -12,10 +12,10 @@ export default function DetailsPage({ searchParams: { skip = 0, limit = 0 } }) {
     <div className="card overflow-hidden">
       <Markdown>{`
 1. [Admin: Check Follow-up rules](/rules)
-1. [Admin: Check Default Follow-up rule](/rules/default)
+2. [Admin: Check Default Follow-up rule](/rules/default)
 `}</Markdown>
 
-      <UseSWRComponent         props={{ skip, limit }} Component={DetailsTable} refreshInterval={60e3}>
+      <UseSWRComponent props={{ skip, limit }} Component={DetailsTable} refreshInterval={60e3}>
         {<DetailsTable {...{ skip, limit }} />}
       </UseSWRComponent>
     </div>
