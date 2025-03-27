16dc5a58b65dd87f63ad6369aedfacb5277bc18d
snomiao <snomiao@gmail.com>
2025-03-27 10:34:41 +0000

    fix(gh-subscriber): init
    init

--- src/ghSubscriber.ts
+++ src/ghSubscriber.ts
@@ -1,44 +1,71 @@
 import { Octokit } from "octokit";
 import sflow from "sflow";
+import { parseUrlRepoOwner } from "./parseOwnerRepo";
 
 const tokens = process.env.GH_SUBSCRIBER_TOKENS?.split(",") || [];
 const gh = (token: string) => new Octokit({ auth: token }).rest;
-// list subscriber users
-console.log(
-  `GH_TOKEN_SUBSCRIBER Users: `,
-  await sflow(tokens)
-    .map((token) => gh(token))
-    .map(async (gh) => {
-      const { data: user } = await gh.users.getAuthenticated();
-      return `${user.login} <${user.email}>`;
-    })
-    .join(", ")
-    .by(new TextEncoderStream())
-    .text(),
-);
 
+// dont await
+ showSubscriberUsers();
 
 if (import.meta.main) {
-  // list subscribed threads
+  
+  await showSubscriberUsers();
+  // subscribeToRepo('')
+  await watchRepo("https://github.com/snomiao/ComfyNode-Registry-test");
+  // await listNotifications()
+}
+
+async function showSubscriberUsers() {
+  console.log(
+    `GH_TOKEN_SUBSCRIBER Users: `,
+    await sflow(tokens)
+      .map((token) => gh(token))
+      .map(async (gh) => {
+        const { data: user } = await gh.users.getAuthenticated();
+        return `${user.login} <${user.email}>`;
+      })
+      .join(", ")
+      .text(),
+  );
+}
+async function listNotifications() {
+  console.log(
+    `Messages: `,
+    await sflow(tokens)
+      .map((token) => gh(token))
+      .map(async (gh) => {
+        const { data: result } = await gh.activity.listNotificationsForAuthenticatedUser();
+        return JSON.stringify(result);
+      })
+      .toArray(),
+  );
+}
+
+// list subscribed threads
+// await sflow(tokens)
+//   .map((token) => gh(token))
+//   .map(async (gh) => {
+//     console.log(await gh.activity.getRepoSubscription({owner: }));
+//   })
+//   .run();
+
+export async function watchRepo(repoUrl: string) {
   await sflow(tokens)
     .map((token) => gh(token))
     .map(async (gh) => {
-      console.log(await gh.activity.listNotificationsForAuthenticatedUser());
-      // await gh.activity.setThreadSubscription({
-      //   thread_id,
-      // });
+      const result = await gh.activity.setRepoSubscription({ ...parseUrlRepoOwner(repoUrl), subscribed: true });
+
+      console.log(result);
     })
     .run();
 }
-
-export async function subscribeTo() {
+export async function unwatchRepo(repoUrl: string) {
   await sflow(tokens)
     .map((token) => gh(token))
     .map(async (gh) => {
-      console.log(await gh.activity.getThreadSubscriptionForAuthenticatedUser());
-      // await gh.activity.setThreadSubscription({
-      //   thread_id,
-      // });
+      const result = await gh.activity.setRepoSubscription({ ...parseUrlRepoOwner(repoUrl), subscribed: false });
+      console.log(result);
     })
     .run();
 }
