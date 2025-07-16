b0eaa76b42c5a70d38a77d5383b690ce3a0065e0
snomiao <snomiao@gmail.com>
2025-07-16 03:51:02 +0000

    fix(slack/index.ts): update import statement for DIE from "phpdie" to ensure correct dependency usage feat(slack/index.ts): log Slack Bot Token for debugging purposes and improve error handling for missing environment variable

--- src/slack/index.ts
+++ src/slack/index.ts
@@ -1,6 +1,8 @@
 import { WebClient } from "@slack/web-api";
-import DIE from "@snomiao/die";
-export const slack = new WebClient(process.env.SLACK_BOT_TOKEN || DIE(new Error("missing env.SLACK_BOT_TOKEN")));
+import DIE from "phpdie";
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN || DIE(("missing env.SLACK_BOT_TOKEN"));
+console.log("Slack Bot Token:", SLACK_BOT_TOKEN)
+export const slack = new WebClient(SLACK_BOT_TOKEN);
 
 if (import.meta.main) {
   await slack.api.test({}); // test token
