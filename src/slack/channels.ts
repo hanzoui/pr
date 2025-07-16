29a3ce23b974521e5b185b5f197849b169eb920e
snomiao <snomiao@gmail.com>
2025-07-16 04:27:38 +0000

    chore(channels.ts): remove unused imports and test token validation to clean up code and improve readability

--- src/slack/channels.ts
+++ src/slack/channels.ts
@@ -1,11 +1,6 @@
-import { WebClient } from "@slack/web-api";
-import DIE from "@snomiao/die";
 import sflow from "sflow";
-export const slack = new WebClient(process.env.SLACK_BOT_TOKEN || DIE(new Error("missing env.SLACK_BOT_TOKEN")));
 
 if (import.meta.main) {
-  await slack.api.test({}); // test token
-  console.log("Slack API token is valid.");
   console.log(JSON.stringify(await getSlackChannel("general")))
 }
 
